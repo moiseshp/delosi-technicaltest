ba9e571be50b013ba25ad292ac4e5386a8d6f858
Moisés Huaringa <moiseseduardo.hp@gmail.com>
2025-01-30 17:29:27 -0500

    fix: config jest

---
+++ src/components/ui/__tests__/text-field.test.tsx
@@ -0,0 +1,8 @@
+import '@testing-library/jest-dom';
+// import { render, screen } from '@testing-library/react';
+
+describe('TextField component', () => {
+  it('example test', () => {
+    expect(true).toBeTruthy();
+  });
+});
